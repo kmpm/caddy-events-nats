60d8673e5584e0849c52e7e0b2682aaa7d0acc79
kmpm <me@kmpm.se>
2024-10-30 19:31:06 +0100

    fix: make usable

--- module.go
+++ module.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"text/template"
 
 	"bytes"
@@ -29,7 +30,9 @@ type NatsHandler struct {
 	nc            *nats.Conn
 	t             *template.Template
 	connected     bool
-	ServerURL     string `json:"url,omitempty"`
+	mustPublish   bool
+	MustPublish   string `json:"must_publish,omitempty"`
+	ServerURL     string `json:"server_url,omitempty"`
 	Subject       string `json:"subject,omitempty"`
 	Username      string `json:"username,omitempty"`
 	Password      string `json:"password,omitempty"`
@@ -46,53 +49,91 @@ func (NatsHandler) CaddyModule() caddy.ModuleInfo {
 
 func (h *NatsHandler) Provision(ctx caddy.Context) error {
 	h.logger = ctx.Logger(h)
-	h.t = template.New(h.Subject)
+	h.logger.Debug("provisioning nats handler")
+	var err error
+	if strings.Contains(h.Subject, "{{") {
+		t := template.New("subject")
+		h.t, err = t.Parse(h.Subject)
+		if err != nil {
+			return fmt.Errorf("failed to parse template: %w", err)
+		}
+	} else {
+		h.t = nil
+	}
+
+	if h.MustPublish == "on" {
+		h.mustPublish = true
+	} else if h.MustPublish == "off" || h.MustPublish == "" {
+		h.mustPublish = false
+	}
 
 	nc, err := nats.Connect(h.ServerURL, h.clientOptions...)
 	if err != nil {
 		return err
 	}
-	h.nc = nc
+	h.logger.Info("nats client connected to server", zap.String("server", nc.ConnectedAddr()))
+
 	//TODO: set client handlers
 	nc.SetErrorHandler(func(conn *nats.Conn, sub *nats.Subscription, err error) {
-		h.logger.Error("nats error", zap.Error(err))
+		h.logger.Error("nats error", zap.Error(err), zap.Bool("connected", h.connected))
 	})
 	nc.SetDisconnectHandler(func(conn *nats.Conn) {
-		h.logger.Info("nats disconnected")
 		h.connected = false
+		h.logger.Info("nats disconnected", zap.Bool("connected", h.connected))
+
 	})
 	nc.SetReconnectHandler(func(conn *nats.Conn) {
-		h.logger.Info("nats reconnected")
 		h.connected = true
+		h.logger.Info("nats reconnected", zap.Bool("connected", h.connected), zap.String("server", conn.ConnectedAddr()))
+
 	})
 	nc.SetClosedHandler(func(conn *nats.Conn) {
-		h.logger.Info("nats closed")
 		h.connected = false
+		h.logger.Info("nats closed", zap.Bool("connected", h.connected))
 	})
 	nc.SetDisconnectErrHandler(func(conn *nats.Conn, err error) {
-		h.logger.Error("nats disconnect error", zap.Error(err))
+		h.connected = false
+		h.logger.Error("nats disconnect error", zap.Error(err), zap.Bool("connected", h.connected))
 	})
+	h.nc = nc
+	h.connected = true
+	h.logger.Debug("nats handler provisioned")
 
 	return nil
 }
 
 func (h *NatsHandler) Handle(ctx context.Context, event caddyevents.Event) error {
 	if !h.connected {
-		return fmt.Errorf("nats not connected")
+		if h.mustPublish {
+			return fmt.Errorf("nats not connected")
+		}
+		h.logger.Warn("nats not connected")
+		return nil
 	}
 	ce := event.CloudEvent()
 	h.logger.Debug("handling event", zap.String("event", ce.ID), zap.String("source", ce.Source))
 	data, err := json.Marshal(ce)
 	if err != nil {
-		return fmt.Errorf("failed to marshal event: %w", err)
+		if h.mustPublish {
+			return fmt.Errorf("failed to marshal event: %w", err)
+		} else {
+			h.logger.Warn("failed to marshal event", zap.Error(err))
+		}
 	}
-	var result bytes.Buffer
-	err = h.t.Execute(&result, ce)
-	if err != nil {
-		return fmt.Errorf("failed to execute template: %w", err)
+	subj := h.Subject
+	if h.t != nil {
+		var result bytes.Buffer
+		err = h.t.Execute(&result, ce)
+		if err != nil {
+			if h.mustPublish {
+				return fmt.Errorf("failed to execute template: %w", err)
+			}
+			h.logger.Warn("failed to execute template", zap.Error(err))
+		}
+		subj = result.String()
 	}
 	m := &nats.Msg{
-		Subject: result.String(),
+		Subject: subj,
 		Data:    data,
 	}
 	header := make(nats.Header)
@@ -103,7 +144,11 @@ func (h *NatsHandler) Handle(ctx context.Context, event caddyevents.Event) error
 
 	err = h.nc.PublishMsg(m)
 	if err != nil {
-		return fmt.Errorf("failed to publish event: %w", err)
+		if h.mustPublish {
+			return fmt.Errorf("failed to publish event: %w", err)
+		} else {
+			h.logger.Warn("failed to publish event", zap.Error(err))
+		}
 	}
 
 	return nil
@@ -116,8 +161,18 @@ func (h *NatsHandler) Validate() error {
 	if h.Subject == "" {
 		return fmt.Errorf("subject is required")
 	}
+	t := template.New("subject")
+	if _, err := t.Parse(h.Subject); err != nil {
+		return fmt.Errorf("failed to parse subject template: %w", err)
+	}
 	//TODO: validate subject format
 
+	if h.MustPublish != "" {
+		//can only be 'on', 'off' or empty
+		if h.MustPublish != "on" && h.MustPublish != "off" {
+			return fmt.Errorf("must_publish must be 'on' or 'off'")
+		}
+	}
 	if h.NKeyFile != "" {
 		opt, err := nats.NkeyOptionFromSeed(h.NKeyFile)
 		if err != nil {
@@ -175,6 +230,11 @@ func (h *NatsHandler) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 			if !d.Args(&h.NKeyFile) {
 				return d.ArgErr()
 			}
+		case "must_publish":
+			if !d.Args(&h.MustPublish) {
+				return d.ArgErr()
+			}
+
 		default:
 			return d.Errf("unknown property '%s'", d.Val())
 		}
